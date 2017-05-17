e5918d78c403a82a9c73180c35b6c6c97903fb25
Erin Corson <frodopwns@gmail.com>
2017-05-17 14:55:47 -0600

    changing up the way the front pafe reacts to mediaqueries

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ChartReposComponent }   from './chart-repos.component';
 import { ReleasesComponent }      from './releases.component';
 import { ChartRepoDetailComponent } from './chart-repo-detail.component';
+import { ReleaseComponent } from './release.component';
 
 const routes: Routes = [
   { path: 'dashboard', redirectTo: '', pathMatch: 'full' },
   { path: 'chart-repos',  component: ChartReposComponent },
+  { path: 'release/:name', component: ReleaseComponent },
   { path: 'chart-repos/detail/:name', component: ChartRepoDetailComponent },
   { path: '',     component: ReleasesComponent }
 ];
