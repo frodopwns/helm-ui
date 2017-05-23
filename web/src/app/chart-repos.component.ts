2dc80bf7de44d1e63d6dd190ca85d56a006d9494
Erin Corson <frodopwns@gmail.com>
2017-05-23 11:55:11 -0600

    using search bar in title section to filter component lists

--- web/src/app/chart-repos.component.ts
+++ web/src/app/chart-repos.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
-
+import {Subscription} from "rxjs/Subscription";
 
 import { ChartRepo } from './chart-repo';
-
 import { ChartRepoService } from './chart-repo.service';
+import { SearchService } from './search.service';
 
 @Component({
   selector: 'chart-repos',
@@ -15,19 +15,32 @@ import { ChartRepoService } from './chart-repo.service';
 export class ChartReposComponent implements OnInit {
 
   repos: ChartRepo[];
+  filtered: ChartRepo[] = [];
   selectedRepo: ChartRepo;
+  searcher: Subscription;
 
   constructor(
     private chartRepoService: ChartRepoService,
     private router: Router,
-    private location: Location
-  ) { }
+    private location: Location,
+    private search: SearchService
+  ) {
+    this.searcher = search.searchSent$.subscribe(
+      terms => {
+        this.filterRepos(terms);
+    });
+  }
 
   ngOnInit(): void {
     this.chartRepoService.getRepos()
-      .then(repos => this.repos = repos);
+      .then(repos => {
+        this.repos = repos;
+        this.filtered = Object.assign([], repos);
+      });
   }
-
+  ngOnDestroy() {
+    this.searcher.unsubscribe();
+  }
   onSelect(chartRepo: ChartRepo): void {
     this.selectedRepo = chartRepo;
   }
@@ -35,7 +48,13 @@ export class ChartReposComponent implements OnInit {
   gotoDetail(chartRepo: ChartRepo): void {
     this.router.navigate(['/chart-repos/detail', chartRepo.name]);
   }
-
+  filterRepos(value: string): void{
+    if(!value) this.filtered = Object.assign([], this.repos);
+    this.filtered = Object.assign([], this.repos).filter(
+        item => (item.name.toLowerCase().indexOf(value.toLowerCase()) > -1 || 
+            item.url.toLowerCase().indexOf(value.toLowerCase()) > -1)
+    );
+  }
   goBack(): void {
     this.location.back();
   }
